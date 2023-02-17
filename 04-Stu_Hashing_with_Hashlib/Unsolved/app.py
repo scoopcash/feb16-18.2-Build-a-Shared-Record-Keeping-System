88d4c7bef1fbcd371b167d0ee35f1549080edb84
scoopcash <114896185+scoopcash@users.noreply.github.com>
2023-02-16 20:18:34 -0500

    uploading exercies

--- 04-Stu_Hashing_with_Hashlib/Unsolved/app.py
+++ 04-Stu_Hashing_with_Hashlib/Unsolved/app.py
@@ -1,8 +1,8 @@
+#Activity 4:
 # Hashing With Hashlib
 ################################################################################
 # In this activity, you’ll use the hashlib library and Streamlit to build an
 # application that can hash any text input.
-
 # Complete the following steps:
 # 1. Create a function to hash an input value.
 # 2. Add a streamlit `text_area` component to accept text from the user.
@@ -14,87 +14,58 @@
 # Imports
 import hashlib
 import streamlit as st
-
 ################################################################################
 # Step 1:
 # Create a function to hash an input value.
-
+#
 # To do so, define a function named
 # `hash_data` that accepts user input, encodes that input, and returns a hash
 # of the data.
-
 # Hint: Inside the `hash_data` function, use the`sha256` function from hashlib
 #  to generate the hash of the data.
-
-# @TODO:
 # Define a function called `hash_data` that takes in a parameter called `data`
-# YOUR CODE HERE!
-
-# @TODO:
-# Instantiate an instance of hashlib's `sha256` function
-sha =  # YOUR CODE HERE!
-
-# @TODO:
-# Use the `encode` function to encode the string version of the data that
-# was passed in as a parameter to the function
-encoded_data =  # YOUR CODE HERE!
-
-# @TODO:
-# Call the hashing instance and the `update` function. Pass it the encoded
-# data as a parameter
-# YOUR CODE HERE!
-
-# @TODO:
-# Return the unique hash of the data using the `hexdigest` function
-# YOUR CODE HERE!
-
+def hash_data(data):
+    # Instantiate an instance of hashlib's `sha256` function
+    sha = hashlib.sha256()
+    # Use the `encode` function to encode the string version of the data that
+    # was passed in as a parameter to the function
+    encoded_data = str(data).encode()
+    # Call the hashing instance and the `update` function. Pass it the encoded
+    # data as a parameter
+    sha.update(encoded_data)
+    # Return the unique hash of the data using the `hexdigest` function
+    return sha.hexdigest()
 ################################################################################
 # Streamlit Code
-
 # Create the application header using a markdown string
 st.markdown("# Create a Unique Hash of Data")
-
 ################################################################################
 # Step 2:
-# Add a Streamlit `text_area` component to accept data from the user. Use the 
+# Add a Streamlit `text_area` component to accept data from the user. Use the
 # Streamlit write function to display the length of the input data back to the user.
-
-# @TODO:
 # Add a Streamlit `text_area` component to accept data from the user
 # Be sure to convert the input data to a string
 # Use the `encode` function to encode the input data
-input_data =  # YOUR CODE HERE!
-
-# @TODO:
+input_data = str(st.text_area("Enter Data")).encode()
 # Use the Streamlit `write` function to display the length (`len`) of the input
 # data back to the user
-# YOUR CODE HERE!
-
+st.write(f"Input Length: {len(input_data)}")
 ################################################################################
 # Step 3:
 # Add a Streamlit `button` named “Hash Text”. When the button is clicked, use
 # the `hash_data` function to first generate a hash of the user's text and then
 # display that hash on the page.
-
-# @TODO:
 # Add a Streamlit `button` named “Hash Text”
-# YOUR CODE HERE!
-
-# @TODO:
-# Generate a hash of the user input using the `hash_data` function
-input_hash =  # YOUR CODE HERE!
-
-# @TODO:
-# Use the Streamlit `write` function to display the unique hash of the data
-# YOUR CODE HERE!
-
-# @TODO:
-# Use the Streamlit `write` function to display the length of the output hash.
-# YOUR CODE HERE!
+if st.button("Hash Text"):
+    # Generate a hash of the user input using the `hash_data` function
+    input_hash = hash_data(input_data)
+    # Use the Streamlit `write` function to display the unique hash of the data
+    st.write(f"Output Hash (fingerprint): {input_hash}")
+    # Use the Streamlit `write` function to display the length of the output hash.
+    st.write(f"Output Length: {len(input_hash)}")
 ################################################################################
 # Step 4:
 # Test the application.
-
 # Complete the following steps:
 # 1. In the terminal, navigate to the `Unsolved` folder for this activity.
 # 2. Be sure that your Conda development environment is active.
@@ -102,5 +73,12 @@ input_hash =  # YOUR CODE HERE!
 # 4. Navigate to [Lorem Ipsum](https://www.lipsum.com/), generate some lorem ipsum, and then paste the generated text.
 # 5. Hash the encoded data by clicking the Hash Text button. Make a note of the unique fingerprint for the data.
 # 6. Change one word of the input text in the text box. Then hash the text again to find out how the hash changes as the input changes.
-
 ################################################################################
+
+
+
+
+
+
+
+
